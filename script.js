1345d82263d7a7fc791761df108c8622d08c1928
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2023-08-25 09:30:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 window.promises = [];
-
-// Do not change the code above this
-// add your promises to the array `promises`
+ 
+// Create an array of five promises with random delays between 1 and 5 seconds
+for (let i = 0; i < 5; i++) {
+  const delay = Math.floor(Math.random() * 5) + 1;
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Promise ${i + 1} resolved after ${delay} seconds`);
+    }, delay * 1000);
+  });
+  promises.push(promise);
+}
+ 
+// Wait for the first promise to resolve using Promise.any()
+Promise.any(promises)
+  .then((result) => {
+    // Print the result to the output div
+    const outputDiv = document.getElementById('output');
+    outputDiv.textContent = result;
+  })
+  .catch((error) => {
+    // Handle errors if no promise resolves
+    console.log('All promises rejected:', error);
+  });
